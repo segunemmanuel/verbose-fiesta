ccd3268fb86f78002700a68cf78d93339d70ab4a
DESKTOP-U5HOMNT\user <segunemmanuel46@gmail.com>
2022-08-22 22:32:11 -0700

    initial commit

---
+++ app/ConsumeExternalServices.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Traits;
+
+use GuzzleHttp\Client;
+trait ConsumeExternalServices{
+
+ /**
+  * Send requesy to anyu external service
+  *@return sdtClass|string
+  */
+
+// resolve api client to be consumed
+  public function makeRequest($method, $requestUrl,$queryParams=[],$formParams=[],$headers=[])
+  {
+     $client= new Client([
+        'base_uri'=>$this->baseUri,
+
+     ]);
+    if(method_exists($this,'resolveAuthorization'))
+    {
+    $this->resolveAuthorization($queryParams,$formParams,$headers);
+
+    }
+    $response=$client->request($method,$requestUrl,[
+    'query'=>$queryParams,
+    'formParams'=>$formParams,
+    'headers'=>$headers,
+    ]);
+
+    $response=$response->getBody()->getContents();
+
+    if(method_exists($this,'decodeResponse'))
+    {
+    $this->decodeResponse($response);
+    }
+    return $response;
+
+  }
+
+}
