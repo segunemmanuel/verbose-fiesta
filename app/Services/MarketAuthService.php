662e39829061a8f4efc2b4a20ca74df9e81c61ef
DESKTOP-U5HOMNT\user <segunemmanuel46@gmail.com>
2022-09-11 13:15:35 -0700

    feat(records)

--- app/Services/MarketAuthService.php
+++ app/Services/MarketAuthService.php
@@ -119,6 +119,31 @@ class MarketAuthService
 
 
 
+    /**
+     * obtain access token from speifici code
+     * @return stdClass
+     */
+    public function getPasswordToken($username,$password)
+    {
+
+        $formParams = [
+            'grant_type' => 'password',
+            'client_id' => $this->passwordClientId,
+            'client_secret' => $this->passwordClientSecret,
+            'username' => $username,
+            'password' => $password,
+            'scope' => 'purchase-product manage-products manage-account read-general',
+        ];
+
+        $tokenData = $this->makeRequest('POST', 'oauth/token', [], $formParams);
+
+        $this->storeValidToken($tokenData, 'password');
+
+        return $tokenData;
+    }
+
+
+
 
 
 
