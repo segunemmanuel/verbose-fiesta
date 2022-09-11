f24eaa41b3235592887606fe97661232c37f283f
DESKTOP-U5HOMNT\user <segunemmanuel46@gmail.com>
2022-09-11 13:59:30 -0700

    feat( Authenticating users directly with email and password)

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,9 +7,11 @@ use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use App\Services\MarketAuthService;
 use App\Services\MarketService;
+use GuzzleHttp\Exception\ClientException;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class LoginController extends Controller
 {
@@ -70,6 +72,53 @@ class LoginController extends Controller
             ->with(['authorizationUrl' => $authorizationUrl]);
     }
 
+
+
+/**
+ * validate login attempts
+ * @param Request $request
+ */
+public function login(Request $request)
+{
+    $this->validateLogin($request);
+
+    // If the class is using the ThrottlesLogins trait, we can automatically throttle
+    // the login attempts for this application. We'll key this by the username and
+    // the IP address of the client making these requests into this application.
+    if ($this->hasTooManyLoginAttempts($request)) {
+        $this->fireLockoutEvent($request);
+
+        return $this->sendLockoutResponse($request);
+    }
+
+    try {
+        $tokenData = $this->marketAuthService->getPasswordToken($request->email, $request->password);
+
+        $userData = $this->marketService->getUserInformation();
+
+        $user = $this->registerOrUpdateUser($userData, $tokenData);
+
+        $this->loginUser($user, $request->has('remember'));
+
+        return redirect()->intended('home');
+    } catch (ClientException $e) {
+        $message = $e->getResponse()->getBody();
+
+        if (Str::contains($message, 'invalid_credentials')) {
+            // If the login attempt was unsuccessful we will increment the number of attempts
+            // to login and redirect the user back to the login form. Of course, when this
+            // user surpasses their maximum number of attempts they will get locked out.
+            $this->incrementLoginAttempts($request);
+
+            return $this->sendFailedLoginResponse($request);
+        }
+
+        throw $e;
+
+    }
+
+
+}
 
     /**
      * resolve the user auth response
